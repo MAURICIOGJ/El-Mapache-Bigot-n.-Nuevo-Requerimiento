abe238f9dde2cb214369ee4e1aab1a615bb3a90a
Lupita-Hernandez <lupita27hv@gmail.com>
2025-09-11 16:29:21 -0600

    first commit

---
+++ src/main/java/mx/edu/caidt/El_Mapache_Bigoton_api/ElMapacheBigotonApiApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.caidt.El_Mapache_Bigoton_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElMapacheBigotonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElMapacheBigotonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/caidt/El_Mapache_Bigoton_api/ElMapacheBigotonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.caidt.El_Mapache_Bigoton_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElMapacheBigotonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
