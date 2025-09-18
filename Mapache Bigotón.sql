a43ace9ebc9ca8d7367d931210196bebe5060e5b
EliseoMD <144739233+EliseoMD@users.noreply.github.com>
2025-09-18 12:09:19 -0600

    Modificación de Mapache Bigotón.sql
    Modificación de las tablas y variables

--- Mapache Bigotón.sql
+++ Mapache Bigotón.sql
@@ -2,21 +2,27 @@ CREATE DATABASE mapache_bigoton;
 USE mapache_bigoton;
 
 CREATE TABLE cliente(
-id int,
+id_cliente bigint,
 nombre Varchar(50),
 telefono Varchar(15)
 );
 
 CREATE TABLE servicio(
-id int,
+id_servicio bigint,
 descripcion Varchar(50),
-costo decimal
+costo double
 );
 
 CREATE TABLE cita(
-id int,
+id_cita bigint,
 fecha date,
 hora time
-)
+);
+
+CREATE TABLE usuario(
+id_usuario bigint,
+nombre Varchar(45),
+contraseña Varchar(45),
+);
 
 
